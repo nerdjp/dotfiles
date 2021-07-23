560dfde47911d45bf46622288626d37ffebf49a3
nerdjp <joaopedrofigm@gmail.com>
2021-07-23 13:39:56 -0300

    update

--- .config/polybar/polybar.sh
+++ .config/polybar/polybar.sh
@@ -9,5 +9,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 MONITOR=eDP1 polybar top & disown
 MONITOR=eDP1 TRAYPOS=center polybar bottom & disown
-# MONITOR=HDMI-1-0 polybar top & disown
-# MONITOR=HDMI-1-0 polybar bottom & disown
+MONITOR=HDMI-1-0 polybar top & disown
+MONITOR=HDMI-1-0 polybar bottom & disown
